66fd03635329c385310afa6a5f8c5d5593d9dc84
JEfrainDR <djoseefrainr@gmail.com>
2021-01-31 10:59:26 -0600

    Pantalla principal
    Adición del activity de la pantalla principal luego del login.

--- app/src/main/java/sv/edu/ues/fia/eisi/fia/MainActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/fia/MainActivity.java
@@ -2,14 +2,27 @@ package sv.edu.ues.fia.eisi.fia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        btnInicio = findViewById(R.id.btn_inicio);
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PantallaPrincipal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
